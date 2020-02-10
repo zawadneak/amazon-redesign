b40f126a06334e6bf7fe60858945f670f6c89d00
Lucas Cassilha <lucaszawadneak@gmail.com>
2020-02-10 17:16:04 -0300

    feature/Login Screen and Pages Organization

---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+import background from '../../assets/img/Background.png';
+
+export default function Login() {
+  return <Container source={background} />;
+}
---
+++ src/pages/User/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+import background from '../../assets/img/Background.png';
+
+export default function User() {
+  return <Container source={background} />;
+}
