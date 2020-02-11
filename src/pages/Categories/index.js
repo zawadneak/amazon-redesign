a0d5fe968e777d419aaf1b8994c37dca15571b75
Lucas Cassilha <lucaszawadneak@gmail.com>
2020-02-11 11:19:45 -0300

    feature/Create Categories and Search Page

--- src/pages/Categories/index.js
+++ src/pages/Categories/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Box, Label } from './styles';
 
 import background from '../../assets/img/Background.png';
 
 export default function Categories() {
-  return <Container source={background} />;
+  return (
+    <Container source={background}>
+      <Box>
+        <Label>Eletronics</Label>
+      </Box>
+      <Box>
+        <Label>Books</Label>
+      </Box>
+      <Box>
+        <Label>Sport</Label>
+      </Box>
+      <Box>
+        <Label>Home</Label>
+      </Box>
+      <Box>
+        <Label>Health</Label>
+      </Box>
+      <Box>
+        <Label>Clothing</Label>
+      </Box>
+      <Box>
+        <Label>Others</Label>
+      </Box>
+    </Container>
+  );
 }
