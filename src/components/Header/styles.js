c12c471f72ab7bbb3578ddff66379096deaf3bff
Lucas Cassilha <lucaszawadneak@gmail.com>
2020-02-11 10:50:44 -0300

    feature/Created Home page and header

---
+++ src/components/Header/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  height: 50px;
+  background-color: #394050;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Holder = styled.View`
+  height: 100%;
+  width: 350px;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const Logo = styled.Image`
+  height: 35px;
+  width: 30px;
+`;
+
+export const Label = styled.Text`
+  margin: 5px;
+`;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -8,3 +8,39 @@ export const Container = styled.ImageBackground`
   align-items: center;
   justify-content: space-evenly;
 `;
+
+export const List = styled.FlatList`
+  align-content: center;
+  height: 300px;
+`;
+
+export const ListBox = styled.TouchableOpacity`
+  align-items: center;
+  height: 300px;
+  margin: 0 20px;
+  width: 300px;
+  background-color: #99a0b2;
+  border-radius: 10px;
+  elevation: 2;
+  box-shadow: 2px 0px 10px rgba(0, 0, 0, 0.25);
+`;
+
+export const ProductImage = styled.Image`
+  margin-top: -20px;
+`;
+
+export const ProductBrand = styled.Image`
+  margin: 5px;
+`;
+
+export const Box = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  height: 160px;
+  margin: 20px;
+  width: 350px;
+  background-color: #99a0b2;
+  border-radius: 10px;
+  elevation: 2;
+  box-shadow: 2px 0px 10px rgba(0, 0, 0, 0.25);
+`;
