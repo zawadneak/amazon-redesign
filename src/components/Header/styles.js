0a74ec1939b4744582fee36216fb0fa8b4ae9bf3
Lucas Cassilha <lucaszawadneak@gmail.com>
2020-02-14 10:00:45 -0300

    feature/Created Cart

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -23,3 +23,55 @@ export const Logo = styled.Image`
 export const Label = styled.Text`
   margin: 5px;
 `;
+
+export const ModalContainer = styled.ImageBackground`
+  flex: 1;
+  padding: 30px;
+  align-items: center;
+`;
+
+export const CartHolder = styled.View`
+  background-color: #99a0b2;
+  width: 350px;
+  height: auto;
+  padding: 20px;
+  border-radius: 4px;
+`;
+
+export const Box = styled.View`
+  height: 100px;
+  width: 230px;
+  align-items: center;
+  flex-direction: row;
+`;
+export const ItemImage = styled.Image`
+  height: 80px;
+  width: 80px;
+  border-radius: 5px;
+`;
+
+export const ItemName = styled.Text`
+  font-size: 15px;
+  margin-left: 5px;
+  font-weight: bold;
+`;
+
+export const ItemAmmount = styled.TextInput`
+  height: 40px;
+  width: 40px;
+  margin: 0 5px;
+  border-radius: 5px;
+  background-color: #fff;
+`;
+
+export const ItemPrice = styled.Text`
+  margin: 5px;
+`;
+
+export const TotalLabel = styled.Text`
+  margin: 10px 0;
+`;
+
+export const Minus = styled.TouchableOpacity``;
+
+export const Plus = styled.TouchableOpacity``;
